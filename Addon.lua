69aee073d1c3beca44ebb796e4a23edd5648be51
Phanx <addons@phanx.net>
2014-10-27 06:05:26 +0000

    - Preliminary 6.0 update

--- Addon.lua
+++ Addon.lua
@@ -14,7 +14,7 @@ local DEFAULT_TEXT = ""
 ------------------------------------------------------------------------
 
 local DIFFICULTY = "Difficulty"
-local LFR, FLEXIBLE, NORMAL, HEROIC, CHALLENGE = "LFR", "FLEX", "N", "H", "C"
+local LFR, NORMAL, HEROIC, MYTHIC, CHALLENGE = "LFR", "N", "H", "M", "C"
 if GetLocale() == "deDE" then
 	DIFFICULTY = "Schwierigkeit"
 	LFR = "SNS"
@@ -23,25 +23,25 @@ elseif GetLocale():match("^es") then
 	LFR, CHALLENGE = "BdB", "D"
 elseif GetLocale() == "frFR" then
 	DIFFICULTY = "Difficulté"
-	LFR, FLEXIBLE, CHALLENGE = "RDR", "DYN", "D"
+	LFR, FLEXIBLE, CHALLENGE = "RDR", "D"
 elseif GetLocale() == "itIT" then
 	DIFFICULTY = "Difficoltà"
-	LFR, FLEXIBLE, HEROIC, CHALLENGE = "RDI", "DIN", "E", "S"
+	LFR, HEROIC, CHALLENGE = "RDI", "E", "S"
 elseif GetLocale() == "ptBR" then
 	DIFFICULTY = "Dificuldade"
-	LFR, CHALLENGE = "PR", "D"
+	LFR, CHALLENGE = "LdR", "D"
 elseif GetLocale() == "ruRU" then
 	DIFFICULTY = "Сложность"
-	LFR, FLEXIBLE, NORMAL, HEROIC, CHALLENGE = "ир", "Гбк", "Н", "Г", "И"
+	LFR, NORMAL, HEROIC, MYTHIC, CHALLENGE = "Пр", "О", "Г", "Э", "И"
 elseif GetLocale() == "koKR" then
 	DIFFICULTY = "난이도"
-	LFR, FLEXIBLE, NORMAL, HEROIC, CHALLENGE = "공찾", "탄력적", "표준", "영웅", "도전" -- needs check
+	LFR, NORMAL, HEROIC, MYTHIC, CHALLENGE = "공찾", "일반", "영웅", "신화", "도전" -- needs check
 elseif GetLocale() == "zhCN" then
 	DIFFICULTY = "难度"
-	LFR, FLEXIBLE, NORMAL, HEROIC, CHALLENGE = "查找", "弹缩", "正常", "英勇", "挑战" -- needs check
+	LFR, NORMAL, HEROIC, MYTHIC, CHALLENGE = "查找", "普通", "英雄", "史诗", "挑战" -- needs check
 elseif GetLocale() == "zhTW" then
 	DIFFICULTY = "難度"
-	LFR, FLEXIBLE, NORMAL, HEROIC, CHALLENGE = "搜尋", "彈性", "普通", "英雄", "挑戰" -- needs check
+	LFR, NORMAL, HEROIC, MYTHIC, CHALLENGE = "搜尋", "普通", "英雄", "傳奇", "挑戰" -- needs check
 end
 
 ------------------------------------------------------------------------
@@ -79,6 +79,25 @@ local obj = LibStub("LibDataBroker-1.1"):NewDataObject("InstanceDifficulty", {
 	end,
 })
 
+local difficultyText = {
+	[2]  = HEROIC,
+	[5]  = HEROIC, -- 10 Player (Heroic)
+	[6]  = HEROIC, -- 25 Player (Heroic)
+	[7]  = LFR,
+	[8]  = CHALLENGE,
+	[11] = HEROIC, -- Heroic Scenario
+	[15] = HEROIC,
+	[16] = MYTHIC,
+	[17] = LFR,
+}
+
+local hideCount = {
+	[7]  = true, -- old LFR
+	[14] = true, -- new Normal
+	[15] = true, -- new Heroic
+	[17] = true, -- new LFR
+}
+
 local f = CreateFrame("Frame")
 f:RegisterEvent("GROUP_ROSTER_UPDATE")
 f:RegisterEvent("GUILD_PARTY_STATE_UPDATED")
@@ -95,7 +114,6 @@ f:SetScript("OnEvent", function(self, event, ...)
 	end
 
 	local _, instanceType, difficulty, _, maxPlayers = GetInstanceInfo()
-	local _, _, isHeroic, isChallengeMode = GetDifficultyInfo(difficulty)
 
 	local color
 	if isGuildGroup then
@@ -107,13 +125,13 @@ f:SetScript("OnEvent", function(self, event, ...)
 	end
 
 	if color and maxPlayers > 0 then
-		if difficulty == 7 then -- LFR
-			obj.text = format("|cff%02x%02x%02x%s|r", color.r * 255, color.g * 255, color.b * 255, LFR)
-		elseif difficulty == 14 then -- Flex
-			obj.text = format("|cff%02x%02x%02x%s|r", color.r * 255, color.g * 255, color.b * 255, FLEXIBLE)
+		if hideCount[difficulty] then
+			-- Flexible raid size, don't show max count
+			obj.text = format("|cff%02x%02x%02x%s|r", color.r * 255, color.g * 255, color.b * 255,
+				difficultyText[difficulty] or NORMAL)
 		else
 			obj.text = format("|cff%02x%02x%02x%s%d|r", color.r * 255, color.g * 255, color.b * 255,
-				isChallengeMode and CHALLENGE or isHeroic and HEROIC or NORMAL, maxPlayers)
+				difficultyText[difficulty] or NORMAL, maxPlayers)
 		end
 	else
 		obj.text = DEFAULT_TEXT
